c1dfe29f9791ec3caaf09f3328b0c6fa38b2c19e
jeffers-surg <114944306+jeffers-surg@users.noreply.github.com>
2022-12-15 16:11:23 -0500

    Added functionality to call functions from command line similar to debug_camera.py.  read_i2c now works

--- image_sensor_test.py
+++ image_sensor_test.py
@@ -1,6 +1,7 @@
 import os
 import time
-
+import sys
+import subprocess
 
 exp_reg_array = [0x3500,0x3501,0x3502]
 group_hold_reg = 0x3208
@@ -22,6 +23,7 @@ binning_enable_reg=0x3820
 
 hbin_ctrl_reg = 0x4501
 
+ref_clock = 24E6
 
 
 def write_i2c(register, value):
@@ -32,10 +34,10 @@ def write_i2c(register, value):
 def read_i2c(register):
 	cmd_str="sudo i2cset -y -f {} {} {} {} && sudo i2cget -y -f {} {}".format(i2c_device, i2c_address, hex((register >> 8) & 0xff), hex(register & 0xff), i2c_device, i2c_address)
 	#print(cmd_str)
-	ret_str=os.system(cmd_str)
-	#ret_str=0x0
-	#print(ret_str)
-	return ret_str
+	#ret_str=os.system(cmd_str)
+	ret_str = subprocess.check_output(cmd_str, shell=True)
+	ret_str = ret_str.decode("utf-8")
+	return int(ret_str.replace("\n",""), 16)
 
 def get_exposure():
 	print("Get exposure")
@@ -218,3 +220,71 @@ def test_binning():
 			hbin_ctrl_set(i, j)
 			standby_exit()
 			time.sleep(5)
+
+pll1_reg_array = [0x0307, 0x0300, 0x0301, 0x0302, 0x0303, 0x0304, 0x0305, 0x3020, 0x3106]
+prediv0_0307 = {"0x0": 1, "0x1": 2}
+prediv_0300 = {"0x0": 1, "0x1": 1.5,"0x2": 2,"0x3": 2.5,"0x4": 3,"0x5": 4,"0x6": 6,"0x7": 8}
+mipi_div_0303 = {"0x0": 1, "0x1": 2, "0x2": 3, "0x3": 4}
+pix_bit_div_0304 = {"0x0": 4, "0x1": 5}
+sys_bit_div_0305 = {"0x0": 4, "0x1": 5}
+pll_pclk_div_3020 = {"0x0": 1, "0x1": 2}
+sys_clk_div_3106 = {"0x0": 1, "0x1": 2, "0x2": 4, "0x3": 8}
+
+def calculate_clock_values():
+
+
+	#reg_vals = [0x00, 0x01, 0x00, 0x32, 0x00, 0x00, 0x01, 0x9b, 0x15]
+
+	reg_vals = []
+	for i in range(len(pll1_reg_array)):
+		reg_vals.append(read_i2c(pll1_reg_array[i]))
+		print("{}: {}".format(hex(pll1_reg_array[i]), hex(reg_vals[i])))
+
+	prediv0_div = prediv0_0307[hex(reg_vals[0] & 0x1)]
+
+	prediv_div = prediv_0300[hex(reg_vals[1] & 0x7)]
+
+	multiplier = ((reg_vals[2] & 0x3) << 8) + (reg_vals[3] & 0xff)
+
+	mipi_div = mipi_div_0303[hex(reg_vals[4] & 0x3)]
+
+	pix_bit_div = pix_bit_div_0304[hex(reg_vals[5] & 0x1)]
+
+	sys_bit_div = sys_bit_div_0305[hex(reg_vals[6] & 0x1)]
+
+	pll_pclk_div = pll_pclk_div_3020[hex((reg_vals[7] >> 3) & 0x1)]
+
+	sys_clk_div = sys_clk_div_3106[hex((reg_vals[8] >> 2) & 0x3)]
+
+	mipi_phy_clock = (ref_clock * multiplier ) / (prediv0_div * prediv_div * mipi_div)
+	mipi_pclk = (ref_clock * multiplier) / (prediv0_div * prediv_div * mipi_div * pix_bit_div * pll_pclk_div)
+	sclk = (ref_clock * multiplier) / (prediv0_div * prediv_div * mipi_div * sys_bit_div* sys_clk_div)
+
+	print("MIPI PHY CLOCK: ", mipi_phy_clock)
+	print("MIPI PIXEL CLOCK: ", mipi_pclk)
+	print("SCLK: ", sclk)
+	
+	return 
+
+_fns = globals().copy()
+for k, v in list(_fns.items()):
+	if k.startswith("_") or not hasattr(v, "__call__") or getattr(v, " __doc__", None) is not None:
+		_fns.pop(k)
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		for arg in sys.argv[1:]:
+			args = []
+			name, _colon, args_str = arg.partition(":")
+			if args_str:
+				args = args_str.split(',')
+			if name not in _fns:
+				print("Error: unknown function '{}'".format(name))
+				help()
+				break
+			print("Running", name, "with args", args)
+			_fns[name](*args)
+	else:
+		help()
+
+
